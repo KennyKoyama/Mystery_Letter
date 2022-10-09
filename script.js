474453bd2ed4a64bd7fc141233404d49e5b60fa2
KennyKoyama <kennysk1982@gmail.com>
2022-10-08 21:30:47 -0300

    Create a function to generate a random list of class for each word

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const textInput = document.querySelector('#carta-texto');
 const createButton = document.querySelector('#criar-carta');
 const letterContent = document.querySelector('#carta-gerada');
+const styles = ['newspaper', 'magazine1', 'magazine2'];
+const sizes = ['medium', 'big', 'reallybig'];
+const rotates = ['rotateleft', 'rotateright'];
+const skews = ['skewleft', 'skewright'];
+const randomNum = (limit) => Math.round(Math.random() * (limit - 1));
+const randomClass = (list) => list[randomNum((list.length))];
+const randomStyle = () => randomClass(styles);
+const randomSize = () => randomClass(sizes);
+const randomRotate = () => randomClass(rotates);
+const randomSkew = () => randomClass(skews);
 const createSpan = () => document.createElement('span');
 const appendSpan = (word) => letterContent.appendChild(word);
 
@@ -16,6 +26,7 @@ createButton.addEventListener('click', () => {
     if (letterContent.innerHTML.length > 0) letterContent.innerHTML += ' ';
     const newSpan = createSpan();
     newSpan.innerHTML = word;
+    newSpan.classList.add(randomStyle(), randomSize(), randomRotate(), randomSkew());
     appendSpan(newSpan);
   });
 });
